76014b31c93cceeb6c850a0ba1556e3c251d4d36
realVato <44781061+realVato@users.noreply.github.com>
2019-10-28 18:07:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>, document.getElementById('root'));
