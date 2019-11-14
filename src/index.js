c6859e9753b8fcd0eb64089f4e4a63f7ed0be574
realVato <44781061+realVato@users.noreply.github.com>
2019-11-14 10:59:42 +0200

    Functional Components Practice

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function MyApp() {
+function MyInfo() {
     return (
-        < ul >
-            <li>1</li> 
-            <li>2</li> 
-            <li>3</li> 
-        </ul>
+        <div>
+            <h1>Tomy G</h1>
+            <p>most charismatic guy in the universe</p>
+            <ul>
+                <li>1</li>
+                <li>2</li>  
+                <li>3</li>
+            </ul>  
+        </div>
     )
 }
 
 ReactDOM.render(
-    <MyApp />,
+    <MyInfo />,
     document.getElementById('root')
     );
