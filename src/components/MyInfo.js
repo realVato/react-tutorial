09759c9b8a2fb2e7d1acb72957a5583769dd7574
realVato <44781061+realVato@users.noreply.github.com>
2019-11-14 11:18:16 +0200

    Move Components into Separate Files

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Tomy G</h1>
+            <p>most charismatic guy in the universe</p>
+            <ul>
+                <li>1</li>
+                <li>2</li>  
+                <li>3</li>
+            </ul>  
+        </div>
+    )
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-function MyInfo() {
-    return (
-        <div>
-            <h1>Tomy G</h1>
-            <p>most charismatic guy in the universe</p>
-            <ul>
-                <li>1</li>
-                <li>2</li>  
-                <li>3</li>
-            </ul>  
-        </div>
-    )
-}
+import MyInfo from './components/MyInfo';
 
 ReactDOM.render(
     <MyInfo />,
