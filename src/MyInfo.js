a246fe3fc5f4f6a70df45c62e6613dc99c1014c0
realVato <44781061+realVato@users.noreply.github.com>
2019-11-15 11:33:41 +0200

    Parent/Child Components

--- src/MyInfo.js
+++ src/MyInfo.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 function MyInfo() {
     return (
         <div>
+            <Header />
             <h1>Tomy G</h1>
             <p>most charismatic guy in the universe</p>
             <ul>
@@ -10,6 +13,7 @@ function MyInfo() {
                 <li>2</li>  
                 <li>3</li>
             </ul>  
+            <Footer />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MyInfo from './components/MyInfo';
+import MyInfo from './MyInfo';
+
 
 ReactDOM.render(
     <MyInfo />,
