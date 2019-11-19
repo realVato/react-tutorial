ca40cfbb17cf912ee5706934ca14acc672a1a3a9
realVato <44781061+realVato@users.noreply.github.com>
2019-11-19 18:51:01 +0200

    Todo App - Phase 2 wip

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import React from 'react'
+import TodoItem from './TodoItem'
 
 function App() {
-
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-    const styles = {
-        fontSize: 48
-    }
-    const buttonStyle = {
-        borderRadius: 5
-    }
-
-    if (hours < 12) {
-        timeOfDay = 'morning'
-        styles.color = 'yellow'
-        buttonStyle.borderColor = 'yellow'
-    } else if (hours > 12 && hours < 17) {
-        timeOfDay = 'good afternoon'
-        styles.color = 'red'
-        buttonStyle.borderColor = 'red'
-    } else {
-        timeOfDay = 'good evening'
-        styles.color = 'blue'
-        buttonStyle.borderColor = 'blue'
-    }
-
     return (
         <div>
-            <h1 style={styles}>Checkboxes</h1>
-            <form className='form'>
-                <div>
-                    <input type='checkbox' /><span>i have paper</span>
-                </div>
-                <div>
-                    <input type='checkbox' /><span>i have fire</span>
-                </div>
-                <div>
-                    <input type='checkbox' /><span>i have lyrics</span>
-                </div>
-                <button style={buttonStyle}>{timeOfDay}</button>
-            </form>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
         </div>
     )
 }
---
+++ src/TodoItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoItem = () => 
+        <div className='to-do-item'>
+            <input type='checkbox' />
+            <p>Placeholder text here</p>
+        </div>
+
+export default TodoItem;
