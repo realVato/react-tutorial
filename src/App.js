746a21961d8835c766523c2af8e706b4b7942e39
realVato <44781061+realVato@users.noreply.github.com>
2019-11-19 18:29:18 +0200

    Inline Styles with the Style Property

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
 
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    const styles = {
+        fontSize: 48
+    }
+    const buttonStyle = {
+        borderRadius: 5
+    }
 
     if (hours < 12) {
         timeOfDay = 'morning'
+        styles.color = 'yellow'
+        buttonStyle.borderColor = 'yellow'
     } else if (hours > 12 && hours < 17) {
         timeOfDay = 'good afternoon'
+        styles.color = 'red'
+        buttonStyle.borderColor = 'red'
     } else {
         timeOfDay = 'good evening'
+        styles.color = 'blue'
+        buttonStyle.borderColor = 'blue'
     }
 
-    return(
+    return (
         <div>
-            <h1>Checkboxes</h1>
+            <h1 style={styles}>Checkboxes</h1>
             <form className='form'>
                 <div>
                     <input type='checkbox' /><span>i have paper</span>
@@ -27,10 +39,10 @@ function App() {
                 <div>
                     <input type='checkbox' /><span>i have lyrics</span>
                 </div>
-                <button>{timeOfDay}</button>
+                <button style={buttonStyle}>{timeOfDay}</button>
             </form>
         </div>
     )
 }
 
-export default App;
+export default App
