1d60832a5f19d91213f166ad6696644045db7422
realVato <44781061+realVato@users.noreply.github.com>
2019-11-20 10:46:17 +0200

    To-do Phase 2

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const TodoItem = () => 
+    <div className='to-do-main'>
         <div className='to-do-item'>
             <input type='checkbox' />
             <p>Placeholder text here</p>
         </div>
+    </div>
 
 export default TodoItem;
