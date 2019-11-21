3ebe535cd73557f54972b53909dc7f9188d91cfe
realVato <44781061+realVato@users.noreply.github.com>
2019-11-21 12:28:26 +0200

    Mapping Components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react'
+
 import Joke from './Joke'
+import jokeData from './jokeData'
 
-const App = () => 
-    <div>
-        <Joke 
-            question = 'What does a nosey pepper do?'
-            answer = 'Gets jalapeno business!'
-        />
-        <Joke 
-            question = 'What do you call a fake noodle?'
-            answer = 'An Impasta'
-        />
-        <Joke 
-            question = ' What do you call an alligator in a vest?'
-            answer = 'An Investigator'
-        />
-        <Joke 
-            question = 'What happens if you eat yeast and shoe polish?'
-            answer = 'Every morning you’ll rise and shine!'
-        />
-        <Joke 
-            question = 'What’s the difference between a guitar and a fish?'
-            answer = 'You can’t tuna fish.'
-        />
-    </div>
+const App = () => {
+    const jokeComponents = jokeData.map(joke => <Joke key={joke.id} question={joke.question} answer={joke.answer} />)
 
+    return (
+        <div>
+            {jokeComponents}
+        </div>
+    )
+}
 
 export default App
--- src/Joke.js
+++ src/Joke.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Joke = (props) => 
-    <div className='aJoke'>
-        <h3>{props.question} {props.answer}</h3>
-    </div>
-
-
+const Joke = (props) => {
+    return (
+        <div className='aJoke'>
+            <h3>{props.question} {props.answer}</h3>
+        </div>
+    )
+}
 
 export default Joke
