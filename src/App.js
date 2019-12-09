30d90b6903a197989a82d5af41ad951df666ece6
realVato <44781061+realVato@users.noreply.github.com>
2019-12-09 20:05:33 +0200

    State

--- src/App.js
+++ src/App.js
@@ -9,42 +9,62 @@ import React from "react"
 //     )
 // }
 
+// CLASS
+
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Header username="CJ"/>
+//                 <Greeting />
+//             </div>
+//         )
+//     }
+//  }
+
+//  class Header extends React.Component {
+//     render(props) {
+//         return (
+//             <header>
+//                 <p>Welcome, {this.props.username}!</p>
+//             </header>
+//         )
+//     }
+//  }
+
+//  class Greeting extends React.Component {
+//      render() {
+//         const date = new Date()
+//         const hours = date.getHours()
+//         let timeOfDay
+
+//         if(hours < 12) {
+//             timeOfDay = 'morning'
+//         } else if (hours >= 12 && hours < 17) {
+//             timeOfDay = 'afternoon'
+//         } else {
+//             timeOfDay = 'night'
+//         }
+//         return (
+//             <h1>Good {timeOfDay} to you, sir or madam!</h1>
+//         )
+//     }
+// }
+
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            answer: "Yes"
+        }
+    }
+    
     render() {
         return (
             <div>
-                <Header username="CJ"/>
-                <Greeting />
+                <h1>Is state important to know? {this.state.answer}</h1>
+                <ChildComponent answer={this.state.answer}/>
             </div>
-        )
-    }
- }
-
- class Header extends React.Component {
-    render(props) {
-        return (
-            <header>
-                <p>Welcome, {this.props.username}!</p>
-            </header>
-        )
-    }
- }
-
- class Greeting extends React.Component {
-     render() {
-        const date = new Date()
-        const hours = date.getHours()
-        let timeOfDay
-
-        if(hours < 12) {
-            timeOfDay = 'morning'
-        } else if (hours >= 12 && hours < 17) {
-            timeOfDay = 'afternoon'
-        } else {
-            timeOfDay = 'night'
-        }
-        return (
-            <h1>Good {timeOfDay} to you, sir or madam!</h1>
         )
     }
 }
