b7841fcae456f93b72bdcf1dd036303510251c31
realVato <44781061+realVato@users.noreply.github.com>
2019-12-12 04:44:04 +0200

    set State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import TodoItem from "./TodoItem"
-import todosData from "./todosData"
+// import TodoItem from "./TodoItem"
+// import todosData from "./todosData"
 
 // function App(props) {
     
@@ -151,22 +151,51 @@ import todosData from "./todosData"
 
 // Phase 5
 
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             todos: todosData
+//         }
+//     }
+    
+//     render() {
+//         const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        
+//         return (
+//             <div className="todo-list">
+//                 {todoItems}
+//             </div>
+//         )    
+//     }
+// }
+
+// set State()
+
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: todosData
+            count: 0
         }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
     }
     
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
-        
         return (
-            <div className="todo-list">
-                {todoItems}
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change!</button>
             </div>
-        )    
+        )
     }
 }
 
