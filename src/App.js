edc8489eab4a55a3e161ea3f6ed6b0c241f2331d
realVato <44781061+realVato@users.noreply.github.com>
2020-01-25 06:37:54 +0200

    setState: Changing state

--- src/App.js
+++ src/App.js
@@ -264,6 +264,35 @@ import todosData from "./todosData"
 //     )
 // }
 
+// setState: Changing state
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             count: 0
+//         }
+//         this.handleClick = this.handleClick.bind(this)
+//     }
+    
+//     handleClick() {
+//         this.setState(prevState => {
+//             return {
+//                 count: prevState.count + 1
+//             }
+//         })
+//     }
+    
+//     render() {
+//         return(
+//             <div>
+//                 <h1>{this.state.count}</h1>
+//                 <button onClick={this.handleClick}>Change!</button>
+//             </div>
+//         )
+//     }
+// }
+
 // set State()
 
 // class App extends React.Component {
